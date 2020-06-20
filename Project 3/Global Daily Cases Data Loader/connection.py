d5be9f86b4e158bb3278e91f5c9d8b37f4083065
joshua farmer <joshuafarmer@joshuas-MBP.lan1>
2020-06-20 17:45:43 -0500

    Added connection strings to project 2/3

--- Project 3/Global Daily Cases Data Loader/connection.py
+++ Project 3/Global Daily Cases Data Loader/connection.py
@@ -1,3 +1,3 @@
 conn_string_proxy = "mysql+pymysql://root:ehaarmanny@127.0.0.1:3306/Covid"
 
-conn_string_deploy = "mysql+pymysql://root:ehaarmanny@/Covid?unix_socket=/cloudsql/covid-api-274519:us-central1:covid-19"
+conn_string_deploy = "mysql+pymysql://root:ehaarmanny@/Covid?unix_socket=/cloudsql/project-3-280623:us-central1:covid-fastapi"
