893840a4d4d243126cb6228c82d9ce8e78172fa3
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-07-01 12:00:44 -0500

    Final test of new Global Date Loader functional and accurate.

--- Project 3/Global Daily Cases Data Loader/app.py
+++ Project 3/Global Daily Cases Data Loader/app.py
@@ -1,10 +1,10 @@
+import time
 import datetime
 import requests
 import csv
-# import pandas as pd
 from contextlib import closing
 import country_converter as coco
-from flask import Flask, make_response
+from flask import Flask, make_response, g
 
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.types import Date, BigInteger, Text
@@ -15,6 +15,7 @@ from sqlalchemy.orm import Session
 from connection import conn_string_proxy, conn_string_deploy
 
 Base = declarative_base()
+
 
 class GlobalDailyCases(Base):
     __tablename__ = "daily_cases"
@@ -33,6 +34,11 @@ class GlobalDailyCases(Base):
 app = Flask(__name__)
 
 
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+
+
 @app.route("/")
 def load():
 
@@ -41,40 +47,47 @@ def load():
     # Change connection_string to conn_string_proxy when connecting locally
     # via proxy and to conn_string_deploy when being uploaded for GCP.
 
-    connection_string = conn_string_proxy
+    connection_string = conn_string_deploy
 
     engine = create_engine(connection_string)
 
     Base.metadata.create_all(engine)
 
     session = Session(bind=engine)
-    
+
     # get most recent upload date to minimized sql transactions
-    most_recent_date = session.query(func.max(GlobalDailyCases.date))
+    most_recent_date = session.query(func.max(GlobalDailyCases.date)).all()[0][0]
 
-    # GlobalDailyCases.__table__.drop(bind=engine)
-
-    coco.logging.getLogger().setLevel(coco.logging.CRITICAL) 
+    coco.logging.getLogger().setLevel(coco.logging.CRITICAL)
 
     ### World Data
     confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
     deaths_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
     recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
-    with closing(requests.get(confirmed_url, stream=True)) as confirmed_r, \
-        closing(requests.get(deaths_url, stream=True)) as deaths_r, \
-        closing(requests.get(recovered_url, stream=True)) as recovered_r:
+    with closing(requests.get(confirmed_url, stream=True)) as confirmed_r, closing(
+        requests.get(deaths_url, stream=True)
+    ) as deaths_r, closing(requests.get(recovered_url, stream=True)) as recovered_r:
 
-        confirmed_csv = csv.reader(confirmed_r.iter_lines(decode_unicode=True), delimiter=",")
+        confirmed_csv = csv.reader(
+            confirmed_r.iter_lines(decode_unicode=True), delimiter=","
+        )
         deaths_csv = csv.reader(deaths_r.iter_lines(decode_unicode=True), delimiter=",")
-        recovered_csv = csv.reader(recovered_r.iter_lines(decode_unicode=True), delimiter=",")
+        recovered_csv = csv.reader(
+            recovered_r.iter_lines(decode_unicode=True), delimiter=","
+        )
 
         csv_headers = list(next(confirmed_csv))
         next(deaths_csv)
         next(recovered_csv)
 
-        reporting_dates = [datetime.datetime.strptime(report_date, "%m/%d/%y").date() for report_date in csv_headers[4:]]
-        most_recent_date_index = reporting_dates.index(most_recent_date) # add try except when checking for index/most recent
+        reporting_dates = [
+            datetime.datetime.strptime(report_date, "%m/%d/%y").date()
+            for report_date in csv_headers[4:]
+        ]
+        most_recent_date_index = reporting_dates.index(
+            most_recent_date
+        )  # add try except when checking for index/most recent
 
         cc = coco.CountryConverter()
 
@@ -91,12 +104,18 @@ def load():
             if country_region == "UK":
                 country_region = "United Kingdom"
 
-            for i, report_date in enumerate(reporting_dates[most_recent_date_index:], start=4+most_recent_date_index):
+            country_region_short = cc.convert(
+                names=[country_region], to="name_short", not_found="n/a"
+            )
+            iso3 = cc.convert(names=[country_region], to="iso3", not_found=None)
+
+            for i, report_date in enumerate(
+                reporting_dates[most_recent_date_index:],
+                start=4 + most_recent_date_index,
+            ):
                 record = GlobalDailyCases(
                     **{
-                        "country_region": cc.convert(
-                            names=[country_region], to="name_short", not_found="n/a"
-                        ),
+                        "country_region": country_region_short,
                         "province_state": province_state,
                         "lat": lat,
                         "long": long,
@@ -104,9 +123,7 @@ def load():
                         "confirmed": confirmed_entry[i],
                         "deaths": deaths_entry[i],
                         "recovered": recovered_entry[i],
-                        "iso3": cc.convert(
-                            names=[country_region], to="iso3", not_found=None
-                        ),
+                        "iso3": iso3,
                     }
                 )
                 session.add(record)
@@ -115,116 +132,9 @@ def load():
     session.close()
     engine.dispose()
 
-    ###########################################################################
-    ############################# Old Below ###################################
-    ###########################################################################
-
-    # confirmed_html = requests.get(confirmed_url).text
-    # confirmed_df = pd.read_html(confirmed_html)[0]
-    # confirmed_df = confirmed_df.iloc[:, 1:]
-    # confirmed_df = confirmed_df.melt(
-    #     id_vars=["Country/Region", "Province/State", "Lat", "Long"]
-    # )
-    # confirmed_df = confirmed_df.rename(
-    #     columns={
-    #         "Country/Region": "country_region",
-    #         "Province/State": "province_state",
-    #         "Lat": "lat",
-    #         "Long": "long",
-    #         "variable": "date",
-    #         "value": "confirmed",
-    #     }
-    # )
-
-    # # Covid-19 Deaths
-    # deaths_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-    # deaths_html = requests.get(deaths_url).text
-    # deaths_df = pd.read_html(deaths_html)[0]
-    # deaths_df = deaths_df.iloc[:, 1:]
-    # deaths_df = deaths_df.melt(
-    #     id_vars=["Country/Region", "Province/State", "Lat", "Long"]
-    # )
-    # deaths_df = deaths_df.rename(
-    #     columns={
-    #         "Country/Region": "country_region",
-    #         "Province/State": "province_state",
-    #         "Lat": "lat",
-    #         "Long": "long",
-    #         "variable": "date",
-    #         "value": "deaths",
-    #     }
-    # )
-
-    # # Covid-19 Recovered
-    # recovered_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-    # recovered_html = requests.get(recovered_url).text
-    # recovered_df = pd.read_html(recovered_html)[0]
-    # recovered_df = recovered_df.iloc[:, 1:]
-    # recovered_df = recovered_df.melt(
-    #     id_vars=["Country/Region", "Province/State", "Lat", "Long"]
-    # )
-    # recovered_df = recovered_df.rename(
-    #     columns={
-    #         "Country/Region": "country_region",
-    #         "Province/State": "province_state",
-    #         "Lat": "lat",
-    #         "Long": "long",
-    #         "variable": "date",
-    #         "value": "recovered",
-    #     }
-    # )
-
-    # # Merge three dataframes
-    # covid_merge1 = pd.merge(confirmed_df, deaths_df, how="outer")
-    # covid_merge2 = pd.merge(covid_merge1, recovered_df, how="outer")
-
-    # # Transform "Date" column into datetime format
-    # covid_merge2["date"] = pd.to_datetime(covid_merge2["date"])
-    # covid_merge2["date"] = covid_merge2["date"].dt.date
-
-    # # Change "UK" for "United Kingdom"
-    # covid_merge2 = covid_merge2.replace(to_replace="UK", value="United Kingdom")
-
-    # # Add column ISO3 to "covid_merge2"
-    # some_names_confirmed = list(covid_merge2.country_region)
-    # standard_names = coco.convert(
-    #     names=some_names_confirmed, to="name_short", not_found="n/a"
-    # )
-    # iso3_codes = coco.convert(names=standard_names, to="iso3", not_found=None)
-    # covid_merge2["iso3"] = iso3_codes
-    # covid_df = covid_merge2
-
-    # # Create a customized dataframe for Sinah's plots
-    # ### Add column ISO3 to "covid_df"
-    # iso_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv"
-    # iso_html = requests.get(iso_url).text
-    # iso_df = pd.read_html(iso_html)[0]
-    # iso3_df = iso_df[["iso3", "Country_Region", "Province_State"]].rename(
-    #     columns={"Country_Region": "country_region", "Province_State": "province_state"}
-    # )
-    # iso3_df = iso3_df.loc[iso3_df["province_state"].isnull()].drop("province_state", 1)
-    # old_pop_df = pd.read_csv("older_pop_2018.csv")
-
-    # covid_df2 = covid_df[
-    #     ["country_region", "date", "province_state", "confirmed", "deaths", "recovered"]
-    # ]
-    # covid_df2 = covid_df2.groupby(["country_region", "date"]).sum().reset_index()
-    # covid_df2["case_fatality"] = round(
-    #     covid_df2["deaths"] / covid_df2["confirmed"] * 100, 2
-    # )
-    # covid_df3 = pd.merge(covid_df2, iso3_df, how="left")
-    # plot_df = pd.merge(covid_df3, old_pop_df, how="left")
-    # plot_df = (
-    #     plot_df.loc[plot_df["date"] >= datetime.date(2020, 3, 1)]
-    #     .reset_index()
-    #     .drop("index", axis=1)
-    # )
-
-    # # Create "daily_cases" table in "Covid" database with "covid_db" dataframe
-    # covid_merge2.to_sql(con=engine, name="daily_cases", if_exists="replace")
-    # plot_df.to_sql(con=engine, name="plotting", if_exists="replace")
-
-    return make_response("Successful", 200)
+    return make_response(
+        f"Successful.\n Load Completion Time: {time.time()-g.request_start_time} s", 200
+    )
 
 
 if __name__ == "__main__":
