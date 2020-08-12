7cf8fb5ab8bbb85b56dad4b4b2744d0adedb511a
edkrueger <edkrueger@gmail.com>
2020-08-12 11:18:05 -0500

    Kill the frontend; API only.

--- Project 3/Front facing/static/javascript.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
-});
